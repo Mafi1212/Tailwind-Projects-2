7c3115f3c170e351207dc668f6c9680f8f5ea5de
Mafi1212 <tarikulmafi175@gmail.com>
2024-07-17 16:45:54 +0600

    full project uplod

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:'class',
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      'display': ['oswald'],
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
